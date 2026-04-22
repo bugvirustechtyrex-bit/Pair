0f768187e3332e5edada64db99bfa9ea46ea157f
bugvirustechtyrex-bit <bugvirustechtyrex@gmail.com>
2026-04-22 10:50:10 +0300

    Update bmb.js

--- bmb.js
+++ bmb.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star SILA-MD
+Don't Forget To Give Star TYREX-MD
 
  Server running on http://localhost:` + PORT)
 })
