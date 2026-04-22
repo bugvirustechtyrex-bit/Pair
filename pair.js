2e150489e64b306c6ac4aa13ba43601ca4ea17a7
bugvirustechtyrex-bit <bugvirustechtyrex@gmail.com>
2026-04-22 10:34:41 +0300

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,193 @@
+const { makeid } = require('./gen-id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router();
+const pino = require("pino");
+const { 
+    default: makeWASocket, 
+    useMultiFileAuthState, 
+    delay, 
+    Browsers, 
+    makeCacheableSignalKeyStore 
+} = require('@whiskeysockets/baileys');
+
+const { upload } = require('./mega');
+
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+
+    async function SILA_MD_PAIR_CODE() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        
+        try {
+            const items = ["Safari", "Chrome", "Firefox"];
+            const randomItem = items[Math.floor(Math.random() * items.length)];
+            
+            let sock = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                generateHighQualityLinkPreview: true,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                syncFullHistory: false,
+                browser: Browsers.macOS(randomItem)
+            });
+            
+            if (!sock.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await sock.requestPairingCode(num);
+                if (!res.headersSent) await res.send({ code });
+            }
+            
+            sock.ev.on('creds.update', saveCreds);
+            
+            sock.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection == "open") {
+                    await delay(3000);
+                    let rf = __dirname + `/temp/${id}/creds.json`;
+
+                    function generateSILA_ID() {
+                        const prefix = "SILA";
+                        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                        let silaID = prefix;
+                        for (let i = prefix.length; i < 22; i++) {
+                            silaID += characters.charAt(Math.floor(Math.random() * characters.length));
+                        }
+                        return silaID;
+                    }
+                    
+                    const silaID = generateSILA_ID();
+
+                    try {
+                        const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+                        let session_code = "sila~" + string_session;
+                        
+                        let code = await sock.sendMessage(sock.user.id, { text: session_code });
+                        
+                        // ===== Message with BOX =====
+                        let desc =`┏━❑ *SILA-MD SESSION* ✅
+┏━❑ *SAFETY RULES* ━━━━━━━━━
+┃ 🔹 *Session ID:* Sent above.
+┃ 🔹 *Warning:* Do not share this code!.
+┃ 🔹 Keep this code safe.
+┃ 🔹 Valid for 24 hours only.
+┗━━━━━━━━━━━━━━━
+┏━❑ *CHANNEL* ━━━━━━━━━
+┃ 📢 Follow our channel: https://whatsapp.com/channel/0029VbBG4gfISTkCpKxyMH02
+┗━━━━━━━━━━━━━━━
+┏━❑ *REPOSITORY* ━━━━━━━━━
+┃ 💻 Repository: https://github.com/Sila-Md/SILA-MD
+┃ 👉 Fork & contribute!
+┗━━━━━━━━━━━━━━━
+
+> © 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐒𝐢𝐥𝐚 𝐓𝐞𝐜𝐡`;
+
+                        await sock.sendMessage(sock.user.id, {
+                            text: desc,
+                            contextInfo: {
+                                externalAdReply: {
+                                    title: 'SILA MD',
+                                    body: '© Sila Tech',
+                                    thumbnailUrl: 'https://files.catbox.moe/36vahk.png',
+                                    thumbnailWidth: 64,
+                                    thumbnailHeight: 64,
+                                    sourceUrl: 'https://whatsapp.com/channel/0029VbBG4gfISTkCpKxyMH02',
+                                    mediaUrl: 'https://files.catbox.moe/36vahk.png',
+                                    showAdAttribution: true,
+                                    renderLargerThumbnail: false,
+                                    previewType: 'PHOTO',
+                                    mediaType: 1
+                                },
+                                forwardedNewsletterMessageInfo: {
+                                    newsletterJid: '120363402325089913@newsletter',
+                                    newsletterName: '© Sila Tech',
+                                    serverMessageId: Math.floor(Math.random() * 1000000)
+                                },
+                                isForwarded: true,
+                                forwardingScore: 999
+                            }
+                        }, { quoted: ddd });
+
+                    } catch (e) {
+                        let ddd = await sock.sendMessage(sock.user.id, { text: e.toString() });
+                        
+                        let desc = `┏━❑ *SILA-MD SESSION* ⚠️
+┏━❑ *SAFETY RULES* ━━━━━━━━━
+┃ 🔹 *Session ID:* Sent above.
+┃ 🔹 *Error:* Session created with minor issues.
+┃ 🔹 Keep this code safe.
+┃ 🔹 Valid for 24 hours only.
+┗━━━━━━━━━━━━━━━
+┏━❑ *CHANNEL* ━━━━━━━━━
+┃ 📢 Follow our channel: https://whatsapp.com/channel/0029VbBG4gfISTkCpKxyMH02
+┗━━━━━━━━━━━━━━━
+┏━❑ *REPOSITORY* ━━━━━━━━━
+┃ 💻 Repository: https://github.com/Sila-Md/SILA-MD
+┃ 👉 Fork & contribute!
+┗━━━━━━━━━━━━━━━
+
+> © 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐒𝐢𝐥𝐚 𝐓𝐞𝐜𝐡`;
+
+                        await sock.sendMessage(sock.user.id, {
+                            text: desc,
+                            contextInfo: {
+                                externalAdReply: {
+                                    title: 'SILA MD',
+                                    body: '© Sila Tech',
+                                    thumbnailUrl: 'https://files.catbox.moe/36vahk.png',
+                                    thumbnailWidth: 64,
+                                    thumbnailHeight: 64,
+                                    sourceUrl: 'https://whatsapp.com/channel/0029VbBG4gfISTkCpKxyMH02',
+                                    mediaUrl: 'https://files.catbox.moe/36vahk.png',
+                                    showAdAttribution: true,
+                                    renderLargerThumbnail: false,
+                                    previewType: 'PHOTO',
+                                    mediaType: 1
+                                },
+                                forwardedNewsletterMessageInfo: {
+                                    newsletterJid: '120363402325089913@newsletter',
+                                    newsletterName: '© Sila Tech',
+                                    serverMessageId: Math.floor(Math.random() * 1000000)
+                                },
+                                isForwarded: true,
+                                forwardingScore: 999
+                            }
+                        }, { quoted: ddd });
+                    }
+
+                    await delay(10);
+                    await sock.ws.close();
+                    await removeFile('./temp/' + id);
+                    console.log(`👤 ${sock.user.id} 🔥 SILA-MD Session Connected ✅`);
+                    await delay(10);
+                    process.exit();
+
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10);
+                    SILA_MD_PAIR_CODE();
+                }
+            });
+            
+        } catch (err) {
+            console.log("⚠️ SILA-MD Connection failed — Restarting service...");
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) await res.send({ code: "❗ SILA-MD Service Unavailable" });
+        }
+    }
+
+    return await SILA_MD_PAIR_CODE();
+});
+
+module.exports = router;
